960b138e2bef344d239f995e644b6a5dc9bfe0be
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-07-11 19:31:06 +0100

    Create layout for shop details

---
+++ src/Data/product.js
@@ -0,0 +1,65 @@
+import Nike from "../assets/nike.jpg";
+import Vans from "../assets/vans.jpg";
+import Converse from "../assets/converse.jpg";
+import Adidas from "../assets/adidas.png";
+
+const Products = [
+  {
+    id: 1,
+    name: "Shoe1",
+    price: "$50",
+    description: "Good shoe",
+    img: Nike
+  },
+  {
+    id: 2,
+    name: "Shoe2",
+    price: "$50",
+    description: "Good shoe",
+    img: Vans
+  },
+  {
+    id: 3,
+    name: "Shoe3",
+    price: "$50",
+    description: "Good shoe",
+    img: Converse
+  },
+  {
+    id: 4,
+    name: "Shoe4",
+    price: "$50",
+    description: "Good shoe",
+    img: Adidas
+  },
+  {
+    id: 5,
+    name: "Shoe5",
+    price: "$50",
+    description: "Good shoe",
+    img: Converse
+  },
+  {
+    id: 6,
+    name: "Shoe6",
+    price: "$50",
+    description: "Good shoe",
+    img: Nike
+  },
+  {
+    id: 7,
+    name: "Shoe7",
+    price: "$50",
+    description: "Good shoe",
+    img: Vans
+  },
+  {
+    id: 8,
+    name: "Shoe8",
+    price: "$50",
+    description: "Good shoe",
+    img: Converse
+  },
+];
+
+export default Products;
--- src/pages/shop/shopdetails.jsx
+++ src/pages/shop/shopdetails.jsx
@@ -1,12 +1,59 @@
 import { useParams } from "react-router-dom"
+import Header from "../../components/Header";
+import Products from "../../Data/product";
+import { useState } from "react";
 
 
 const ShopDetails = () => {
   const {name} = useParams();
 
+  // find the product that matches the name from the URL parameters
+  const getProducts = Products.find((product) => product.name === name);
+
+  const [addProduct, setAddProduct] = useState();
+  const [subtractProduct, setSubtractProduct] = useState();
+
   return (
-    <div>shopdetails { name }</div>
+    <>
+      <Header />
+      
+      <div className="flex-container">
+        {getProducts && (
+          <>
+            <div>
+              <img src={getProducts.img} alt={getProducts.name} />
+            </div>
+
+            <div>
+              <h2>{getProducts.name}</h2>
+              <p>{getProducts.price}</p>
+
+              <div>
+                {getProducts.description}
+              </div>
+
+              <div>
+                <div>
+                  <button>-</button>
+                  <span></span>
+                  <button>+</button>
+                </div>
+
+                <div>
+                  <button>
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+
+            </div>
+          </>
+
+        )}
+
+      </div>
+    </>
   )
 }
 
-export default ShopDetails
+export default ShopDetails;
